c2b2ea7f054a425a20419a179887a51108f5ba45
Abhay123456758 <abhay4752.be23@chitkara.edu.in>
2024-10-05 08:01:04 -0300

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCGpLSdINOIBgBnL2GfYyY1GGwvgPLS3f4",
+  authDomain: "task7-1p-23dc9.firebaseapp.com",
+  projectId: "task7-1p-23dc9",
+  storageBucket: "task7-1p-23dc9.appspot.com",
+  messagingSenderId: "667544030457",
+  appId: "1:667544030457:web:abc5a9632c54c22527a628"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
